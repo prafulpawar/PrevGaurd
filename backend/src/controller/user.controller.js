a6ecd9d9ab8118354f2c31e5ec9d7e7f55b0d807
prafulpawar <prafuldukhi@gmail.com>
2025-04-01 15:13:02 +0530

    __DONE__

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,11 +1,50 @@
-module.exports.userCreation = async(req,res)=>{
-     try{
-           const {username,email,password} =req.body;
-           
-     }
-     catch(error){
-        return res.status(401).json({
-            message:"Error In Creation "
+const userModel = require("../models/user.model");
+
+module.exports.userCreation = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(401).json({
+                message: "All Fields Must Be Required"
+            })
+        }
+        // find if user exists 
+        const isExists = await userModel.findOne({
+            $or: [
+                { email },
+                { username }
+            ]
         })
-     }
+
+        // if exists
+        if (isExists) {
+            return res.status(401).json({
+                messae: "User Is Alredy Exists"
+            })
+        }
+
+        // hashpassword
+        const EncPassowrd = await userModel.hashPassword(password);
+
+        // not exists then lets create the user
+        const user = await userModel.create({
+            username,
+            email,
+            password:EncPassowrd
+        });
+
+        // now accessToken 
+        const accessToken  = user.accessToken();
+        const refershToken = user.refershToken();
+
+        
+
+
+
+    }
+    catch (error) {
+        return res.status(401).json({
+            message: "Error In Creation "
+        })
+    }
 }
