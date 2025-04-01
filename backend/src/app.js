8638a5bf021ad4efe6b5475b9d6b4c730fe48813
prafulpawar <prafuldukhi@gmail.com>
2025-04-01 14:27:12 +0530

    __DONE__

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config()
+
 const express = require('express');
 const connectDB = require('../src/utils/db');
 connectDB()
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config()
 module.exports = app;
--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 const connectDB = async(req,res)=>{
       try{
-          console.log(config.MONO_URI)
+         
            const db = await mongoose.connect(config.MONGO_URI);
-           console.log(`Connected To Database ${db}`)
+           console.log(`Connected To Database `)
       }
       catch(error){
           console.log(error);
