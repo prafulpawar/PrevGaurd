fe90093d2d14144ac000796a3a6d3d73e405adac
prafulpawar <prafuldukhi@gmail.com>
2025-04-01 14:43:40 +0530

    __DONE__

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,8 @@
 const _config = {
-    PORT:process.env.PORT,
-    MONGO_URI:process.env.MONGO_URI
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    ACCESS_TOKEN:process.env.ACCESS_TOKEN,
+    REFERSH_TOKEN:process.env.REFERSH_TOKEN
 }
 
 const config = Object.freeze(_config);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,8 +5,35 @@ const userSchema = new monggoose.Schema({
         minlength:['Minimum Length Is 6',6],
         maxlength:['Maximum Length Is 6',6],
         required:true
+     },
+
+     role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user',
+     },
+
+     email:{
+        type:String,
+        minlength:['Minimum Length Is 6',10],
+        maxlength:['Maximum Length Is 6',50],
+        required:true
+     },
+     password:{
+        type:String,
+        minlength:['Minimum Length Is 6',6],
+        maxlength:['Maximum Length Is 6',20],
+        required:true 
+     },
+     refershToken:{
+        type:String
      }
 })
+
+
+
+
+
 
 const userModel = monggoose.model('User',userSchema);
 module.exports  = userModel; 
