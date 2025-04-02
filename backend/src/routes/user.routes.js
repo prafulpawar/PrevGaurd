b61ae4b04a6e50a2ab3847bdaf9c1c33f0b09647
prafulpawar <prafuldukhi@gmail.com>
2025-04-02 22:08:26 +0530

    __DONE__

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {registerUser,verfifyOtp}  = require('../controller/user.controller');
-const {rateLimiter} = require('../middlewares/rateLimiter')
+const rateLimiter = require('../middlewares/rateLimiter')
 
 router.post('/register',rateLimiter,registerUser);
 router.post('/verifyOtp',rateLimiter,verfifyOtp);
