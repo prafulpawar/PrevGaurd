4145d864e8b3973865756b1872d059ff2524038c
prafulpawar <prafuldukhi@gmail.com>
2025-04-02 17:12:12 +0530

    __DONE_

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config()
-
 const express = require('express');
 const connectDB = require('../src/utils/db');
 connectDB()
+const router = require('./routes/user.routes');
+
+
+app.use('/user',router);
+
 const app = express();
 module.exports = app;
