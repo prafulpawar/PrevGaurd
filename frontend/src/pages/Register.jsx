e9db08de78bfc7364f2c62c23f2e032539ad6939
prafulpawar <prafuldukhi@gmail.com>
2025-04-04 01:03:43 +0530

    __DONE__

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-
 function Register() {
+   const [formData , setFormData] = ({ 
+        username:"",
+        email:"",
+        password:"",
+   })
 
-    
+   const handleSubmit = (e)=>{
+       e.preventDefault()
+   }
+
+
+
   return (
 
     <div>
-         <form >
+         <form onSubmit={handleSubmit} >
                <input type="text"  placeholder='Enter Your Username'/>
                <input type="password" placeholder='Enter Your Password' />
                <input type="email" placeholder='Enter Your Email' />
