f119e1af64d8635332085bf1f371967a9b5cc3b3
prafulpawar <prafuldukhi@gmail.com>
2025-04-04 22:26:19 +0530

    __DONE__

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -8,7 +8,9 @@ router.post('/auth/signup',rateLimiter,registerUser);
 router.post('/verifyOtp',rateLimiter,verfifyOtp);
 router.get("/otp-status", getOtpStatus);
 router.post('/auth/login',loginUser)
+
 router.get('/auth/me',isAuth,getUserInfo)
+
 router.get('/auth/logout',logoutUser)
 
 module.exports = router;
