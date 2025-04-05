dde4fd673fbeb8f7c2466f346717bfc19826c642
prafulpawar <prafuldukhi@gmail.com>
2025-04-05 19:42:48 +0530

    __DONE__

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,14 @@ connectDB()
 const router = require('./routes/user.routes');
 const rebbitMQ = require('./services/rabbitMQ')
 const appAPI = express();
-
+const cors = require('cors')
 
 appAPI.set('trust proxy', true);
 
 
 appAPI.use(express.json());
 appAPI.use(express.urlencoded({extended:true}));
-
+appAPI.use(cors())
 appAPI.use((req, res, next) => {
     console.log('Client IP:', req.ip);
     console.log('Forwarded IPs:', req.ips);
