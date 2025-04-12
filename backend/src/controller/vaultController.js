dc0867afe1f1cee2e66b694fbbf5df97be798418
prafulpawar <prafuldukhi@gmail.com>
2025-04-12 23:50:13 +0530

    __DONE__

--- backend/src/controller/vaultController.js
+++ backend/src/controller/vaultController.js
@@ -1,6 +1,15 @@
 module.exports.createVaultController = async(req,res)=>{
     try{
+         const {password} = req.body;
+         if(!password){
+              return res.status(400).json({
+                message:"Can Get Password"
+              })
+         }
          
+
+
+
     }
     catch(error){
         return res.status(400).json({
