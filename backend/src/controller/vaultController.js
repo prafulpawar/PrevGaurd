a77963e95acd4a2245ffb9d76b7be44af99d9822
prafulpawar <prafuldukhi@gmail.com>
2025-04-14 01:38:12 +0530

    __DONE__

--- backend/src/controller/vaultController.js
+++ backend/src/controller/vaultController.js
@@ -1,3 +1,4 @@
+const bcrypt  = require('bcryptjs')
 module.exports.createVaultController = async(req,res)=>{
     try{
          const {password} = req.body;
@@ -7,8 +8,7 @@ module.exports.createVaultController = async(req,res)=>{
               })
          }
          // Hashing Of Password
-         
-         
+        const hashedPassword = await bcrypt.hash(password,10);
     }
     catch(error){
         return res.status(400).json({
