57ebd50d6c4d4bc743fc5b29018e8bceda8856ca
prafulpawar <prafuldukhi@gmail.com>
2025-04-15 00:29:19 +0530

    __DONE__

--- backend/src/controller/breachController.js
+++ backend/src/controller/breachController.js
@@ -1,22 +1,23 @@
-const BreachModel = require("../models/breachData");
+const axios = require('axios');
+
 module.exports.brechController = async (req, res) => {
     try {
-        const { email } = req.body;
-        const breachedData = await BreachModel.findOne({ email }).select('-date -__v ')
-        if(breachedData === null){
-              
-        }
+        const { emailData } = req.body;
+
+       
+        const response = await axios.get(`https://api.xposedornot.com/v1/check-email/${emailData}`);
+        const BreachData = response.data;
+
+    
         return res.status(200).json({
-            breachedData,
-            message: "SuccessFully"
-        })
+            BreachData,
+            message: "Successfully fetched breach data"
+        });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({
+            message: "Error in getting breach data",
+            error: error.message
+        });
     }
-    catch (error) {
-        console.log(error)
-        return res.status(
-            400
-        ).json({
-            message: "Error In Getting Data"
-        })
-    }
-}
+};
