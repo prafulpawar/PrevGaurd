dc1910e06d26327604564c1793f60706da71647f
prafulpawar <prafuldukhi@gmail.com>
2025-04-18 23:50:59 +0530

    __DONE__

--- backend/src/routes/sharedRoute.js
+++ backend/src/routes/sharedRoute.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router  = express.Router()
 const {addsharedData,getsharedData,updatesharedData,deleteSharedData} = require('../controller/sharedDataController');
 const { verifyAuth } = require('../middlewares/isAuth');
-router.get('/api/data',verifyAuth , getsharedData);
-router.post('/api/data', verifyAuth, addsharedData)
-router.put('/api/data/:id',verifyAuth,updatesharedData)
-router.delete('/api/data/:id',verifyAuth,deleteSharedData)
+
+router.get('/dash/data',verifyAuth , getsharedData);
+router.post('/dash/data', verifyAuth, addsharedData)
+router.put('/dash/data/:id',verifyAuth,updatesharedData)
+router.delete('/dash/data/:id',verifyAuth,deleteSharedData)
+
 module.exports = router;
