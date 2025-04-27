03516b5c9be39040ffcbab18ee604e5a645dac79
prafulpawar <prafuldukhi@gmail.com>
2025-04-27 22:07:22 +0530

    __DONE__

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,19 +8,3 @@ export default defineConfig({
     tailwindcss()
   ],
 })
-
-// import { defineConfig } from 'vite';
-// // import react from '@vitejs/plugin-react';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss()
-//   ],
-//   // build: {
-//   //   rollupOptions: {
-//   //     external: ['react-redux'], // Externalize react-redux
-//   //   }
-//   // }
-// });
