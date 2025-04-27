b5e0fc1d1b83768ebce9fe80662a535a42014cb4
prafulpawar <prafuldukhi@gmail.com>
2025-04-27 21:37:34 +0530

    __DONE__

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-redux'], // Externalize react-redux
+    }
+  }
+});
